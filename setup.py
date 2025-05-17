701f7da9d1765fa0e2e4bab06e15880d685b6dcd
ShivamShende2608 <140915731+ShivamShende2608@users.noreply.github.com>
2025-05-17 17:11:16 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version='0.0.0',
+    author='Shivam Shende',
+    author_email='shivamshende2608@gmail.com',
+    packages= find_packages(),
+    install_requires=[],
+)
